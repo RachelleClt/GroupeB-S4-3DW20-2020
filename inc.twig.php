9f40ad94e6a67951e0f56eb27ea498a0c5e29e4b
TheJoker6677 <=>
2020-10-22 21:26:31 +0200

    Ajout commentaire dans le inc.twig.php

--- inc.twig.php
+++ inc.twig.php
@@ -1,8 +1,14 @@
 <?php
+	//On inclut Autoloader qui permet de charger nos autres pages
 	include_once('lib/Twig/Autoloader.php');
 
+	//Charge les classes Twig
 	Twig_Autoloader::register();
 
+	//On charge les fichiers .tpl du dossier templates
 	$templates = new Twig_Loader_Filesystem('templates');
+
+	//Object central du moteur Twig a chaque création d'appplication effectué avec le moteur Twig
+	//celui-ci crée un environement qui permet d'avoir un loader qui permet lui de charger les templates
 	$twig      = new Twig_Environment($templates);//
  
